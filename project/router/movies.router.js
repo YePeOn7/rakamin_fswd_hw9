56cf5b11fe3a9a58e8d0e84a683c8f358120179f
Yohan <prakoso.yohan@gmail.com>
2024-03-08 17:52:34 +0800

    add CRUD

--- project/router/movies.router.js
+++ project/router/movies.router.js
@@ -1,20 +1,116 @@
 const pool = require('../config/db_config.js');
 const router = require('express').Router();
 
+const moviesTable = "movies"
+
 router.get("/", (req, res) => {
-    res.send("get movies");
+    pool.query(`SELECT * FROM ${moviesTable}`, (err, result) => {
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else{
+            res.status(200).send(result.rows);
+        }
+    })
+});
+
+router.get("/:id", (req, res) => {
+    pool.query(`SELECT * FROM ${moviesTable} WHERE id=$1`, [req.params.id], (err, result) => {
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(404).json({message: `There is no data with id: ${req.params.id}`});
+        }
+        else{
+            res.send(result.rows);
+        }
+    });
 });
 
 router.post("/", (req, res) => {
-    res.send("post movies");
+    const title = req.query.title;
+    const genres = req.query.genres;
+    const year = req.query.year;
+
+    if( !title || !genres || !year ){
+        res.status(400).json({message: "Missing parameter(s)"});
+        return;
+    }
+    
+    pool.query(`
+    INSERT INTO ${moviesTable}(title, genres, year)
+    VALUES($1, $2, $3)`,
+    [title, genres, year], (err, result) => {
+        if(err){
+            res.status(500).json({message: "Internal server Error"});
+        }
+        else{
+            res.status(201).json({message: "Data has been added succesfully"});
+        }
+    })
 });
 
 router.put("/", (req, res) => {
-    res.send("put movies");
+    const title = req.query.title;
+    const genres = req.query.genres;
+    const year = req.query.year;
+    const id = req.query.id;
+
+    if( !title || !genres || !year || !id){
+        res.status(400).json({message: "Missing parameter(s)"});
+        return;
+    }
+    
+    pool.query(`SELECT * FROM ${moviesTable} WHERE id=$1`, [id], (err, result) =>{
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(404).json({message: `There is no data with id: ${id}`});
+        }
+        else{
+            pool.query(`
+            UPDATE ${moviesTable}
+            SET title=$1, genres=$2, year=$3
+            WHERE id=$4`,
+            [title, genres, year, id], (err, result) => {
+                if(err){
+                    res.status(500).json({message: "Internal server Error"});
+                }
+                else{
+                    res.status(200).json({message: "Data has been updated succesfully"});
+                }
+            })
+        }
+    })
+
 });
 
 router.delete("/", (req, res) => {
-    res.send("delete movies");
+    const id = req.query.id;
+
+    pool.query(`SELECT * FROM ${moviesTable} WHERE id=$1`, [id], (err, result) =>{
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(404).json({message: `There is no data with id: ${id}`});
+        }
+        else{
+            pool.query(`
+            DELETE FROM ${moviesTable}
+            WHERE id=$1`,
+            [id], (err, result) => {
+                if(err){
+                    res.status(500).json({message: "Internal server Error"});
+                }
+                else{
+                    res.status(200).json({message: `Data with id: ${id} has been deleted succesfully`});
+                }
+            })
+        }
+    })
 });
 
 module.exports = router;
--- project/router/users.router.js
+++ project/router/users.router.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const usersTable = "users";
 
 router.get("/", (req, res) => {
-    pool.query(`SELECT * FROM users`, (err, result) => {
+    pool.query(`SELECT * FROM ${usersTable}`, (err, result) => {
         if(err){
             res.json({message: "Internal Server Error"}).status(500);
         }
@@ -12,18 +12,113 @@ router.get("/", (req, res) => {
             res.send(result.rows);
         }
     });
+})
+
+router.get("/:id", (req, res) => {
+    pool.query(`SELECT * FROM ${usersTable} WHERE id=$1`, [req.params.id], (err, result) => {
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(404).json({message: `There is no data with id: ${req.params.id}`});
+        }
+        else{
+            res.send(result.rows);
+        }
+    });
 });
 
-router.post("/", (req, res) => {
-    res.send("post users");
+router.post("/", (req, res) => {  
+    const email = req.query.email;
+    const gender = req.query.gender;
+    const password = req.query.password;
+    const role = req.query.role;
+
+    if(!email || !gender || !password || !role){
+        res.status(400).json({message: "Missing parameter(s)"});
+        return;
+    }
+
+    pool.query(`
+        INSERT INTO ${usersTable}(email, gender, password, role)
+        VALUES ($1, $2, $3, $4);`, [email, gender, password, role],
+        (err, result) => {
+        if(err){
+            if(err.code === '23505'){
+                res.status(406).json({message: `The email ${email} has been used`});
+            }
+            else{
+                res.status(500).json({message: "Internal Server Error", error: err});
+            }
+        }
+        else{
+            res.status(201).json({message: "User created successfully"});
+        }   
+    });
 });
 
 router.put("/", (req, res) => {
-    res.send("put users");
+    const id = req.query.id;
+    const email = req.query.email;
+    const gender = req.query.gender;
+    const password = req.query.password;
+    const role = req.query.role;
+
+    if(!email || !gender || !password || !role || !id){
+        res.status(400).json({message: "Missing parameter(s)"});
+        return;
+    }
+    
+    pool.query(`SELECT * FROM ${usersTable} WHERE id=${id}`, (err,result) => {
+        if(err){
+            res.status(500).json({message: "Internal server error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(500).json({message: `There is no data with id: ${id}`});
+        }
+        else{
+            pool.query(`
+                UPDATE ${usersTable}
+                SET email=$1, gender=$2, password=$3, role=$4
+                WHERE id = $5;`, 
+                [email, gender, password, role, id],
+                (err, result) => {
+                if(err){
+                    res.status(500).json({message: "Internal Server Error", err: err});
+                }
+                else{
+                    res.status(200).json({message: "User updated successfully"});
+                }   
+            });
+        }
+    })
+
 });
 
 router.delete("/", (req, res) => {
-    res.send("delete users");
+    const id = req.query.id;
+
+    pool.query(`SELECT * FROM ${usersTable} WHERE id=$1`, [id], (err, result) =>{
+        if(err){
+            res.status(500).json({message: "Internal Server Error"});
+        }
+        else if(result.rowCount == 0){
+            res.status(404).json({message: `There is no data with id: ${id}`});
+        }
+        else{
+            pool.query(`
+            DELETE FROM ${usersTable}
+            WHERE id=$1`,
+            [id], (err, result) => {
+                if(err){
+                    res.status(500).json({message: "Internal server Error"});
+                }
+                else{
+                    res.status(200).json({message: `Data with id: ${id} has been deleted succesfully`});
+                }
+            })
+        }
+    })
 });
 
 module.exports = router;
