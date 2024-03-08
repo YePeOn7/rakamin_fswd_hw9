d0bf589e858631241cc468e4a5df005a05438dac
Yohan <prakoso.yohan@gmail.com>
2024-03-08 19:30:21 +0800

    update

--- project/app.js
+++ project/app.js
@@ -4,14 +4,24 @@ const express = require('express');
 
 const app = express();
 
+// Application level middle ware
+app.use((req, res, next) => {
+    console.log(`Time: ${Date.now()}`);
+    next();
+});
+
 app.get("/", (req, res) => {
-    res.send("here");
+    res.send("ok");
 });
 
 pool.connect((err, res) => {
     if(err){
         console.log("Error ===> ", err);
     }
+});
+
+app.use((req, res) => {
+    res.status(404).send("Not Found");
 });
 
 app.use(route);
